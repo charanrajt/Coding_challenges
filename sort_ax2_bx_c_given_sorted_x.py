5ab1f06710ae525260b181e217e8fb4499d1dd27
charanraj thimmisetty <charanrajt@gmail.com>
2017-08-25 13:22:21 -0700

    Create  sort_ax2_bx_c_given_sorted_x.py

---
+++ sort_ax2_bx_c_given_sorted_x.py
@@ -0,0 +1,49 @@
+def sort_ax2_bx_c_given_sorted_x(x,a,b,c):
+    func = lambda x: a*x**2+b*x+1 
+    arg_min_max=-b/(2.*a)  #2*a*x+b=0, x =-b/2a if a>0 it is min else max
+    list1=[i for i in x if i<=arg_min_max]
+    list2=[i for i in x if i>arg_min_max]
+    flist1=map(func,list1)
+    flist2=map(func,list2)
+    result=[]; 
+    if a>0:
+        flist1.reverse()
+    elif a<0:
+        flist2.reverse()
+
+    while len(flist1)>0 and len(flist2)>0 :
+        if flist1[0]>flist2[0]:
+           result.append(flist2[0])
+           flist2.pop(0)
+        else:
+          result.append(flist1[0])
+          flist1.pop(0)  
+    if flist1:
+        [result.append(i) for i in flist1]
+    if flist2:
+        [result.append(i) for i in flist2]  
+              
+    return result
+
+def is_sorted(lst):
+        return lst == sorted(lst)           
+     
+
+#Checking the code for a>0
+a=20
+b=0
+c=1
+import numpy as np
+x=[-2,-1,0,1,2]
+Fx=sort_ax2_bx_c_given_sorted_x(x,a,b,c)
+print Fx
+print is_sorted(Fx)         
+
+#Checking the code for a>0
+a=-40
+b=2
+c=1
+x=sorted(np.random.randn(100))
+Fx=sort_ax2_bx_c_given_sorted_x(x,a,b,c)
+
+print is_sorted(Fx)
