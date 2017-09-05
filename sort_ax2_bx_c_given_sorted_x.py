00a9cc3ff64ec26f3c7edb7363a023498ca82ab2
charanraj thimmisetty <charanrajt@gmail.com>
2017-09-05 11:03:24 -0700

    Update sort_ax2_bx_c_given_sorted_x.py

--- sort_ax2_bx_c_given_sorted_x.py
+++ sort_ax2_bx_c_given_sorted_x.py
@@ -1,15 +1,25 @@
 def sort_ax2_bx_c_given_sorted_x(x,a,b,c):
+    """
+    Goal: Given sorted list x, output sorted of F:=a*x**2+b*x+c
+    
+    """
     func = lambda x: a*x**2+b*x+1 
     arg_min_max=-b/(2.*a)  #2*a*x+b=0, x =-b/2a if a>0 it is min else max
-    list1=[i for i in x if i<=arg_min_max]
-    list2=[i for i in x if i>arg_min_max]
+    list1=[i for i in x if i<=arg_min_max] #Functions has symetry w.r.t to arg_min_max
+    list2=[i for i in x if i>arg_min_max] # F(List1) is decreasing in a>0
     flist1=map(func,list1)
     flist2=map(func,list2)
     result=[]; 
-    if a>0:
-        flist1.reverse()
+    if a==0:
+       if b<0:
+        result=map(func,x);
+        result.reverse()
+       elif b>0:
+        result=map(func,x); 
+    elif a>0:
+        flist1.reverse() #a>0  F(List1) is decreasing  so reverse it 
     elif a<0:
-        flist2.reverse()
+        flist2.reverse() #a  F(List2) is decreasing  so reverse it 
 
     while len(flist1)>0 and len(flist2)>0 :
         if flist1[0]>flist2[0]:
